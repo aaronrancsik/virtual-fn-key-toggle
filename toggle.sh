d20981af42dc429f6db8a8e9c03bed0aa6d838db
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-01-30 11:50:32 +0100

    refactoring

--- toggle.sh
+++ toggle.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/bash
+
 doNotTouchDir=$(dirname "$0")'/doNotTouch'
 mkdir -p $doNotTouchDir
-
 LOCKDIR="${doNotTouchDir}/lock"
-
-echo $LOCKDIR
-
-function cleanup {
-    if rmdir $LOCKDIR; then
-        echo "Finished"
-    else
-        echo "Failed to remove lock directory '$LOCKDIR'"
-        exit 1
-    fi
-}
-
-
 
 function On(){
     custonPath="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/"
@@ -44,7 +31,6 @@ function On(){
     # Update the list of bindings for the shortcuts to work
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "$overallbindings"
 }
-
 function Off(){
     layouts=($(gsettings get org.gnome.desktop.input-sources sources | grep -Eoh "[\'][a-z]{2}[\']"))
     IFS=","
@@ -74,26 +60,36 @@ function Off(){
     trunkedBindings="[${trunkedBindings:2}]"
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "$trunkedBindings"
 }
-
-if mkdir $LOCKDIR; then
-    #Ensure that if we "grabbed a lock", we release it
-    #Works for SIGTERM and SIGINT(Ctrl-C)
-    trap "cleanup" EXIT
-
-    echo "Acquired lock, running"
-    if test -f $doNotTouchDir/on
-    then 
-        # It is on, so lets turn off
-        rm $doNotTouchDir/on
-        Off
-        $(dirname "$0")/osd/osd.sh "FN KEY RELEASED" "changes-allow-symbolic"
+function cleanup {
+    if rmdir $LOCKDIR; then
+        echo "Finished"
     else
-        # Its is off, so lets turn on
-        touch $doNotTouchDir/on
-        On
-        $(dirname "$0")/osd/osd.sh "FN KEY LOCKED" "changes-prevent-symbolic"
+        echo "Failed to remove lock directory '$LOCKDIR'"
+        exit 1
     fi
-else
-    echo "Could not create lock directory '$LOCKDIR'"
-    exit 1
-fi
+}
+function ThreadSaveToggle(){
+    if mkdir $LOCKDIR; then
+        #Ensure that if we "grabbed a lock", we release it
+        #Works for SIGTERM and SIGINT(Ctrl-C)
+        trap "cleanup" EXIT
+
+        echo "Acquired lock, running"
+        if test -f $doNotTouchDir/on
+        then 
+            # It is on, so lets turn off
+            rm $doNotTouchDir/on
+            Off
+            $(dirname "$0")/osd/osd.sh "FN KEY RELEASED" "changes-allow-symbolic"
+        else
+            # Its is off, so lets turn on
+            touch $doNotTouchDir/on
+            On
+            $(dirname "$0")/osd/osd.sh "FN KEY LOCKED" "changes-prevent-symbolic"
+        fi
+    else
+        echo "Could not create lock directory '$LOCKDIR'"
+        exit 1
+    fi
+}
+ThreadSaveToggle
